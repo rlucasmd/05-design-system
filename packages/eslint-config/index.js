32894552e0a38b1b1c375e1ca057a43ed3027478
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-06-14 22:17:53 -0300

    first commit

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["@rocketseat/eslint-config/react"],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: false,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+      },
+    ],
+  },
+  ignorePatterns: ["*.json", "./dist/*.*"]
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,35 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from "@ignite-ui/tokens";
+import { createStitches, defaultThemeMap } from "@stitches/react";
+export const {
+  css,
+  globalCss,
+  getCssText,
+  styled,
+  theme,
+  createTheme,
+  config,
+  keyframes,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: "space",
+    width: "space",
+  },
+  theme: {
+    fonts,
+    colors,
+    radii,
+    lineHeights,
+    fontSizes,
+    space,
+    fontWeights,
+  },
+});
