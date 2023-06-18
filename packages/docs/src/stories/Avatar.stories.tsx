e6addffd0fd9d0725202f49b9b23f5d3025b7932
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-06-17 23:43:31 -0300

    add: Avatar component stories

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Avatar, AvatarProps } from "@ignite-ui/react";
+
+export default {
+  title: "Data display/Avatar",
+  component: Avatar,
+  args: {
+    src: "https://github.com/ranieri3232.png",
+    alt: "Ranieri Lucas",
+  },
+} as Meta<AvatarProps>;
+
+export const Primary: StoryObj<AvatarProps> = {};
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
